9faa39831e9f5f68dd94b6c67fd2f7c037d4058a
Devashish289 <61451684+Devashish289@users.noreply.github.com>
2020-04-26 21:00:04 +0530

    Add files via upload

---
+++ Spring labs/LAB-1/src/main/java/com/sunsoft/d/EmployeeService.java
@@ -0,0 +1,5 @@
+package com.sunsoft.d;
+
+public interface EmployeeService {
+public Employee getDetails(int empId);
+}
---
+++ Spring labs/LAB-1/src/main/java/com/sunsoft/d/EmployeeServiceImpl.java
@@ -0,0 +1,10 @@
+package com.sunsoft.d;
+
+public class EmployeeServiceImpl implements EmployeeService{
+
+	public Employee getDetails(int empId) {
+		EmployeeDAO emp=new EmployeeDAO();
+		return emp.getDetails(empId);
+	}
+ 
+}
